77ffc4a4f1dd6a6aa2e48431be53b3ede3b82b74
Brett Evans <brett.evans@me.com>
2015-09-22 15:03:48 -0600

    Fix cd Typo

--- scripts/atlas.sh
+++ scripts/atlas.sh
@@ -7,7 +7,7 @@ mkdir ../tmp
 cd ../tmp
 wget https://github.com/hashicorp/atlas-upload-cli/releases/download/v0.2.0/atlas-upload-cli_0.2.0_linux_amd64.tar.gz
 tar -xzvf atlas-upload-cli_0.2.0_linux_amd64.tar.gz
-cd ../fresh-food-connect-
+cd ../fresh-food-connect-api
 git stash
 ./../tmp/atlas-upload-cli_0.2.0_linux_amd64/atlas-upload -vcs -debug codefordenver/fresh-food-connect .
 
