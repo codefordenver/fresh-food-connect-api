92245d82de0c980e04e18d65ab679f2623756252
Brett Evans <brett.evans@me.com>
2015-09-21 19:41:10 -0600

    Install Atlas Upload Manually

--- scripts/atlas.sh
+++ scripts/atlas.sh
@@ -1,18 +1,27 @@
 #! /usr/bin/env sh
 
-# Update Packages and Install Go
-# apt-get update
-# apt-get install golang
+set -ex
+wget https://github.com/hashicorp/atlas-upload-cli/releases/download/v0.2.0/atlas-upload-cli_0.2.0_linux_amd64.tar.gz -O /tmp/atlas-upload-cli.tar.gz
+tar -xzvf /tmp/atlas-upload-cli.tar.gz
+./tmp/atlas-upload-cli/atlas-upload -vcs -debug codefordenver/fresh-food-connect .
 
-# Set GOPATH
-export GOPATH="/usr/share/go/"
 
-# Get Atlas Upload CLI
-cd ..
-git clone https://github.com/hashicorp/atlas-upload-cli.git
-cd atlas-upload-cli
-make
-cd ../fresh-food-connect-api
 
-# Upload Rails Application to Atlas
-../atlas-upload-cli/bin/atlas-upload -vcs -debug codefordenver/fresh-food-connect .
+
+
+# # Update Packages and Install Go
+# # apt-get update
+# # apt-get install golang
+
+# # Set GOPATH
+# export GOPATH="/usr/share/go/"
+
+# # Get Atlas Upload CLI
+# cd ..
+# git clone https://github.com/hashicorp/atlas-upload-cli.git
+# cd atlas-upload-cli
+# make
+# cd ../fresh-food-connect-api
+
+# # Upload Rails Application to Atlas
+# ../atlas-upload-cli/bin/atlas-upload -vcs -debug codefordenver/fresh-food-connect .
