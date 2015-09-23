eeee5678c159553af26250e6c1c6f1d5f88123cc
Brett Evans <brett.evans@me.com>
2015-09-23 16:10:46 -0600

    Debugging Permissions

--- packer/scripts/rails.sh
+++ packer/scripts/rails.sh
@@ -12,3 +12,13 @@ gem install bundler --no-rdoc --no-ri
 
 # Install NodeJS for Rails Asset Compiler
 sudo apt-get install -y nodejs && sudo ln -sf /usr/bin/nodejs /usr/local/bin/node
+
+# Install Dependencies
+cd /ops/app
+bundle install --production --without development test
+
+# Compile Rails Assets and Run Database Migrations
+bundle exec rake assets:precompile db:migrate
+
+# Start Thin
+bundle exec thin start
