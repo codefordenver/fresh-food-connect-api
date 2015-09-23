fec1b3dd3fd73696c4d89c4fbee331f15333bb37
Jennifer Chng <jmchng10@gmail.com>
2015-09-22 21:51:16 -0600

    Updated db schema from migrations; rename reminder email method name

--- app/controllers/api/v1/emails_controller.rb
+++ app/controllers/api/v1/emails_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::EmailsController < Api::V1::BaseController
   before_action :ensure_admin_user
 
-  def send_welcome
+  def create
   	users = User.all
     ConfirmationMailer.send_confirmation(users).deliver
   end
