dc4ac9352f37a1131f58ff720d4a7f0b78a872ad
Brett Evans <brett.evans@me.com>
2015-09-30 14:18:38 -0500

    Change How Postgres User is Created

--- packer/scripts/postgres.sh
+++ packer/scripts/postgres.sh
@@ -2,7 +2,9 @@
 sudo apt-get install -y postgresql postgresql-contrib
 
 # Create the Postgres User Role
-sudo -u postgres psql postgres postgres -c "ALTER USER postgres WITH ENCRYPTED PASSWORD 'postgres'"
+su - postgres
+create role rails with createdb login password 'rails';
 
+#sudo -u postgres psql postgres postgres -c "ALTER USER postgres WITH ENCRYPTED PASSWORD 'postgres'"
 # Switch to Postgres User
 # sudo -i -u postgres
