85f19a20690dd594834e35808684bff0cec602a6
Brett Evans <brett.evans@me.com>
2015-10-01 13:05:28 -0600

    Provision Environment Variables with Packer

--- packer/scripts/postgres.sh
+++ packer/scripts/postgres.sh
@@ -1,9 +1,14 @@
 # Install Postgres
 sudo apt-get install -y postgresql postgresql-contrib
 
-# Create the Postgres User Role
-su - postgres
-create role rails with createdb login password 'rails';
+echo "CREATE ROLE $POSTGRES_USER WITH LOGIN PASSWORD '$POSTGRES_PASSWORD';" | sudo -i -u postgres psql
+sudo -i -u postgres createdb --owner=$POSTGRES_USER $POSTGRES_ENV
+
+# user_name=demo_user
+# password=pass1
+# echo "CREATE ROLE $user_name WITH LOGIN PASSWORD '$password';" | sudo -i -u postgres psql
+# sudo -i -u postgres createdb --owner=$user_name demo_rails_app_app
+
 
 #sudo -u postgres psql postgres postgres -c "ALTER USER postgres WITH ENCRYPTED PASSWORD 'postgres'"
 # Switch to Postgres User
