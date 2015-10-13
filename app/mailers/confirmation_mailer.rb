210aa6eb59c0e63b744649162b8c35dc4aa1499b
Shelby Switzer <shelbyswitzer@gmail.com>
2015-10-12 20:09:05 -0600

    add ability to specify locations or users to send donation confirmation email to:

--- app/mailers/confirmation_mailer.rb
+++ app/mailers/confirmation_mailer.rb
@@ -1,22 +1,23 @@
 class ConfirmationMailer < MandrillMailer::TemplateMailer
   default from: ENV['FROM_EMAIL_ADDRESS']
 
-  def send_confirmation(users)
-    users = Array.wrap(users) # in case you pass a single user object
-    emails = users.map{|user| {email: user.email, name: "#{user.first} #{user.last}"}}
+  def send_confirmation(locations)
+    locations = Array.wrap(locations) # in case you pass a single location object
+    emails = locations.map{|l| l.user.email}
 
     mandrill_mail(
       template: 'Donation Reminder',
       to: emails,
       important: true,
       inline_css: true,
-      recipient_vars: users.map do |user|
-        location_id = user.locations.first.try(:id) # in case for some reason user doesnt have any locations, this will just return nil instead of erroring
-        { user.email =>
+      recipient_vars: locations.map do |location|
+        # not sure how the following will map to the above :to email addresses if there are more than one locations per user email
+        # a better alternative to ensure the emails get sent in that scenario is to send the emails individually in a background worker
+        # but I don't know what the deployment situation is so am uncertain about ensuring sidekiq gets started and stays running
+        { location.user.email =>
           {
-            "FIRST_NAME" => user.first,
-            "USER_YES_DONATION_URL" => confirm_yes_url(user.id, location_id),
-            "USER_NO_DONATION_URL" => confirm_no_url(location_id)
+            "USER_YES_DONATION_URL" => confirm_yes_url(location.id),
+            "USER_NO_DONATION_URL" => confirm_no_url(location.id)
           }
         }
       end
@@ -25,7 +26,7 @@ class ConfirmationMailer < MandrillMailer::TemplateMailer
 
   # I realize this isn't great but rails url helpers aren't available here
   # so I'm just hacking this together until a more elegant solution can be figured out post pilot
-  def confirm_yes_url(user_id, location_id)
+  def confirm_yes_url(location_id)
     default_host + "/confirm-donation?location_id=#{location_id}"
   end
 
